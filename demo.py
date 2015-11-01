f330b1ca09926ae87ba1af912a92801c904d4a37
Jose Araya <joseguillermo.araya@gmail.com>
2015-11-01 18:54:07 -0300

    cambio de estructura a la del demo

---
+++ demo.py
@@ -0,0 +1,108 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''
+Este script resuelve un problema simple de diffusion en 1D.
+La ecuación a resover es:
+    dT/dt = d2T/dx2; T(0,x) = sin(pi * x); T(t, 0) = T(t, 1) = 0
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def inicializa_T(T, N_steps, h):
+    '''
+    Rellena T con las condiciones iniciales del problema.
+    Se asegura que las condiciones en los bordes sean cero.
+    '''
+    for i in range(N_steps):
+        x = i * h
+        T[i] = np.sin(np.pi * x)
+    T[0] = 0
+    T[-1] = 0
+
+
+def calcula_b(b, N_steps, r):
+    for j in range(1, N_steps - 1):
+        b[j] = r * T[j+1] + (1-2*r) * T[j] + r * T[j-1]
+
+
+def calcula_alpha_y_beta(alhpa, beta, b, r, N_Steps):
+    Aplus = -1 * r
+    Acero = (1 + 2 * r)
+    Aminus = -1 * r
+    alpha[0] = 0
+    beta[0] = 0  # viene de la condicion de borde T(t, 0) = 0
+    for i in range(1, N_steps):
+        alpha[i] = -Aplus / (Acero + Aminus*alpha[i-1])
+        beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
+
+
+def avanza_paso_temporal(T, T_next, alpha, beta, N_steps):
+    T_next[0] = 0
+    T_next[-1] = 0
+    for i in range(N_steps - 2, 0, -1):
+        T_next[i] = alpha[i] * T_next[i+1] + beta[i]
+    # T = T_next.copy() # Esto no funciona, hacerlo fuera de la funcion
+
+# Main
+
+# setup
+N_steps = 41
+N_pasos_temporales = 100
+
+h = 1 / (N_steps - 1)
+# dt = h**2 / 2 # Este es el máximo teórico para el metodo explicito
+dt = 0.01
+r = dt / 2 / h**2
+
+T = np.zeros(N_steps)
+T_next = np.zeros(N_steps)
+
+b = np.zeros(N_steps)
+alpha = np.zeros(N_steps)
+beta = np.zeros(N_steps)
+
+inicializa_T(T, N_steps, h)
+
+# Queremos guardar las soluciones en cada paso
+T_solucion = np.zeros((N_pasos_temporales, N_steps))
+T_solucion[0, :] = T.copy()
+
+for i in range(1, N_pasos_temporales):
+    calcula_b(b, N_steps, r)
+    calcula_alpha_y_beta(alpha, beta, b, r, N_steps)
+    avanza_paso_temporal(T, T_next, alpha, beta, N_steps)
+    T = T_next.copy()
+    T_solucion[i, :] = T.copy()
+
+
+# Plots
+
+# ejemplo 1
+
+x = np.linspace(0, 1, N_steps)
+
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(111)
+
+for i in range(0, N_pasos_temporales, 10):
+    ax.plot(x, T_solucion[i, :])
+ax.set_ylim(0, 1)
+
+# ejemplo 2
+# usar el plano x, t y plotear T en la 3a dimension
+fig2 = plt.figure(2)
+fig2.clf()
+ax2 = fig2.add_subplot(111)
+y = np.arange(0, N_pasos_temporales) * dt
+X, Y = np.meshgrid(x, y)
+ax2.pcolormesh(X, Y, T_solucion)
+
+plt.show()
+plt.draw()
--- tarea.py
+++ tarea.py
@@ -14,93 +14,93 @@ def funcion_inicial(x):
     return np.exp(- x ** 2 / 0.1)
 
 
-def iniciar_solucion(t, x):
-    '''inicia la forma de la matriz con ceros, cada fila correspondera a una
-    solución para n(x), donde la fila i es la solución para tiempo i.
-    t es la cantidad de pasos temporales y x la de pasos espaciales'''
-    return np.zeros((t+1, x+1))
-
-
-def poner_condiciones_iniciales():
+def poner_condiciones_iniciales(S, N, h):
     '''llena la fila 0 de la matriz con la condición inicial,
     retrona la nueva matriz'''
-    x = np.linspace(X_IN, X_FIN, numero_pasos_x + 1)
+    x = np.linspace(X_IN, X_FIN, N)
     vfuncion_inicial = np.vectorize(funcion_inicial) # vectorizar funcion
     fila_inicial = vfuncion_inicial(x)
-    solucion[0, :] = fila_inicial
+    S = fila_inicial
+    S[0] = BORDE_1
+    S[-1] = BORDE_2
+    return S
 
 
-def poner_condiciones_borde(solucion, N, N_t):
-    '''define condiciones rígidas de borde para todo t, recibe la
-    cantidad de pasos en x(N), y la cantidad de pasos en t (N_t)'''
-    for i in range(N_t + 1):
-        solucion[i, N] = BORDE_2
-        solucion[i, 0] = BORDE_1
-
-
-def b(t, k):
+def calcula_b(b, N, r):
     '''lado derecho (todo lo que depende del tiempo anterior
-    al que se quiere calcular), para cada punto k, en el tiempo t - 1'''
-    b_valor = (solucion[t - 1, k+1] * r + solucion[t - 1, k-1] * r +
-               solucion[t - 1, k] * (MU * (1 - solucion[t - 1, k]) - 2 * r))
-    return b_valor
+    al que se quiere calcular), para cada punto k'''
+    for k in range(1, N - 1):
+        b[k] = (S[k+1] * r + S[k-1] * r +
+               S[k] * (e * MU * (1 - S[k]) + 1 - 2 * r))
 
 
-def encontrar_alfa_beta(N, t):
+def encontrar_alfa_beta(alfa, beta, b, r,N ):
     ''' encuentra  todos los alfas y betas para el tiempo t dado'''
-    alfa = np.zeros(N)         # iniciar parametros alfa y beta
-    beta = np.zeros(N)
-    alfa[N - 1] = 0                    # condiciones finales para alfa y beta
-    beta[N - 1] = BORDE_2
+    A_mas = -1 * r
+    A_menos = -1* r
+    A_0 = (1 + 2 * r)
+    alfa[0] = 0                # condiciones finales para alfa y beta
+    beta[0] = BORDE_2
 
-    for k in range(int(N) - 1, 0, -1): # iterar para llenar valores de alfa y beta
-        alfa[k-1] = - A_menos / (A_mas * alfa[k] + A_0)
-        beta[k-1] = (b(t, k) - A_mas * beta[k]) / (A_mas * alfa[k] + A_0)
-    return alfa, beta
+    for k in range(1, N): # iterar para llenar valores de alfa y beta
+        alfa[k] = -1 * A_mas / (A_menos * alfa[k-1] + A_0)
+        beta[k] = (b[k] - A_menos * beta[k-1]) / (A_menos * alfa[k-1] + A_0)
 
 
-def avance_temporal(sol, N, t_actual):
+def avance_temporal(S, S_next, alfa, beta, N):
     '''avanza un paso de la iteracion temporal, es decir, llena la columna
-    llamada con t_actual'''
-    alfa, beta = encontrar_alfa_beta(N, t_actual)
-    for k in range(0, int(N) - 1, int(N)):
-        sol[t_actual, k + 1] = alfa[k] * sol[t_actual, k] + beta[k]
+    llamada con t'''
+    S_next[0] = BORDE_1
+    S_next[-1] = BORDE_2
+    for k in range(int(N) - 2, 0, -1):
+        S_next[k] = alfa[k] * S_next[k+1] + beta[k]
 
 
 def mostar_resultado(sol):
     '''grafica el cada fila de sol vs x en el mismpo plot'''
     fig, ax = plt.subplots()
-    x_values = np.linspace(X_IN, X_FIN, numero_pasos_x + 1)
+    x_values = np.linspace(X_IN, X_FIN, numero_x)
     ax.set_ylim(0, 1)
     ax.set_xlim(0, 1)
-    ax.plot(x_values, np.transpose(sol))
+    for i in range(0, numero_t):
+        ax.plot(x_values, sol[i, :], color="r")
     plt.show()
 
-
+# Main
 # Setup
 T_FIN = 4       # tiempo final
 T_IN = 0        # tiempo inicial
 X_FIN = 1       # extremo 2
 X_IN = 0        # extremo 1
-numero_pasos_t = 99
-numero_pasos_x = 499
-e = (T_FIN - T_IN) / numero_pasos_t
-h = (X_FIN - X_IN) / numero_pasos_x
+numero_t = 100       # cantidad de valores en t
+numero_x = 500   # cantidad de valores en x
+numero_pasos_t = numero_t - 1
+numero_pasos_x = numero_x - 1
+e = (T_FIN - T_IN) / (numero_pasos_t)
+h = (X_FIN - X_IN) / (numero_pasos_x)
 GAMMA = 0.001
 MU = 1.5
 BORDE_1 = 1     # condicion de borde 1
 BORDE_2 = 0     # condicion de borde 2
 r = (GAMMA * e) / (2 * h ** 2)
 
-A_menos = -1 * r
-A_0 = 1 + 2 * r
-A_mas = -1 * r
+S = np.zeros(numero_x)
+S_next = np.zeros(numero_x)
 
-# Main
-solucion = iniciar_solucion(numero_pasos_t, numero_pasos_x)
-poner_condiciones_iniciales()
-poner_condiciones_borde(solucion, numero_pasos_x, numero_pasos_t)
-print(solucion[:,-1])
-for i in range(int(numero_pasos_t)):
-    avance_temporal(solucion, numero_pasos_x, i+1)
-mostar_resultado(solucion)
+b = np.zeros(numero_x)
+alfa = np.zeros(numero_x)
+beta = np.zeros(numero_x)
+
+S = poner_condiciones_iniciales(S, numero_x, h)
+
+S_solucion = np.zeros((numero_t, numero_x))
+S_solucion[0, :] = S.copy()
+
+for i in range(1, numero_t):
+    calcula_b(b, numero_x, r)
+    encontrar_alfa_beta(alfa, beta, b, r, numero_x)
+    avance_temporal(S, S_next, alfa, beta, numero_x)
+    S = S_next.copy()
+    S_solucion[i, :] = S.copy()
+
+mostar_resultado(S_solucion)
