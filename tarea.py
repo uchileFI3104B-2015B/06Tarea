993c762f5020484e8c95848abd28757b795a849a
Jose Araya <joseguillermo.araya@gmail.com>
2015-10-31 22:49:43 -0300

    implementacion decondicion inicial

--- tarea.py
+++ tarea.py
@@ -9,18 +9,20 @@ import pdb
 '''Script para resolver numéricamente la ecuación de Fisher-KPP, usando
 Crank Nicolson '''
 e = 0.1     # paso temporal
-h = 1 / 500 # paso espacial
+h = 1 / 10 # paso espacial
 GAMMA = 0.001
 NU = 1.5
 T_FIN = 4   # tiempo final
 T_IN = 0    # tiempo inicial
-X_FIN = 0   # extremo 2
-X_IN = 1    # extremo 1
+X_FIN = 1   # extremo 2
+X_IN = 0    # extremo 1
 BORDE_1 = 1 # condicion de borde 1
 BORDE_2 = 0 # condicion de borde 2
 
-def funcion_inicial():
-    '''función que representa la condicion inicial'''
+def funcion_inicial(x):
+    '''función que representa la condicion inicial, recibe un x, y devuelve
+    el valor de la función evaluada'''
+    return np.exp(- x ** 2 / 0.1)
 
 
 
@@ -31,17 +33,26 @@ def iniciar_solucion(t, x):
 
 
 def poner_condiciones_iniciales():
-    '''llena la fila 0 de la matriz con la condición inicial'''
+    '''llena la fila 0 de la matriz con la condición inicial,
+    retrona la nueva matriz'''
+    x = np.linspace(X_IN, X_FIN, numero_pasos_x + 1)
+    vfuncion_inicial = np.vectorize(funcion_inicial) # vectorizar funcion
+    fila_inicial = vfuncion_inicial(x)
+    solucion[0, :] = fila_inicial
 
 
 def iterar_crank_nicolson():
     '''avanza un paso de la iteracion, es decir, llena la columna siguiente'''
 
-        
+
+def mostar_resultado():
+    '''  grafica el resultado'''
+
+
 # Main
 numero_pasos_t = (T_FIN - T_IN) / e
 numero_pasos_x = (X_FIN - X_IN) / h
 solucion = iniciar_solucion(numero_pasos_t, numero_pasos_x)
-solucion = poner_condiciones_iniciales()
-solucion = iterar_crank_nicolson()
+poner_condiciones_iniciales()
+# solucion = iterar_crank_nicolson()
 mostar_resultado()
