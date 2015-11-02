63161164d89a86124b63849b1d8358b9c20f87a4
[Felipe Castillo] <felipe.castillo.t@ing.uchile.cl>
2015-11-02 20:42:16 -0300

    Pegué y edité el ejemplo C-N del profesor

--- Solution_to_Fisher-KPP.py
+++ Solution_to_Fisher-KPP.py
@@ -3,6 +3,46 @@
 '''
 Este script es para resolver la ecuación de Fisher-KPP
 '''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
 #-implementar Crank-N
+
+def inicializa_T(T, N_steps, h):
+    '''
+    Rellena T con las condiciones iniciales del problema.
+    Se asegura que las condiciones en los bordes sean cero.
+    '''
+    for i in range(N_steps):
+        x = i * h
+        T[i] = np.exp((-x**2) / 0.1) 
+    T[0] = 0
+    T[-1] = 0
+
+
+def calcula_b(b, N_steps, r):
+    for j in range(1, N_steps - 1):
+        b[j] = r * T[j+1] + (1-2*r) * T[j] + r * T[j-1]
+
+
+def calcula_alpha_y_beta(alhpa, beta, b, r, N_Steps):
+    Aplus = -1 * r
+    Acero = (1 + 2 * r)
+    Aminus = -1 * r
+    alpha[0] = 0
+    beta[0] = 0  # viene de la condicion de borde T(t, 0) = 0
+    for i in range(1, N_steps):
+        alpha[i] = -Aplus / (Acero + Aminus*alpha[i-1])
+        beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
+
+
+def avanza_paso_temporal(T, T_next, alpha, beta, N_steps):
+    T_next[0] = 0
+    T_next[-1] = 0
+    for i in range(N_steps - 2, 0, -1):
+        T_next[i] = alpha[i] * T_next[i+1] + beta[i]
+
 #-implementar Euler explícito
 #-Main, Condiciones iniciales, condiciones de bordes, busca y guarda las soluciones en cada paso, plotea
