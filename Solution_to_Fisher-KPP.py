02c97a4f5e026f167369aaa83836725a0a05b96b
[Felipe Castillo] <felipe.castillo.t@ing.uchile.cl>
2015-11-02 20:39:00 -0300

    Algoritmo a implementar

---
+++ Solution_to_Fisher-KPP.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Este script es para resolver la ecuación de Fisher-KPP
+'''
+#-implementar Crank-N
+#-implementar Euler explícito
+#-Main, Condiciones iniciales, condiciones de bordes, busca y guarda las soluciones en cada paso, plotea
