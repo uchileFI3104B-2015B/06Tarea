1b6a6135b74dc235aca7eadecab5a0fe8f24d05e
TatiFlores <tatiflores.4@gmail.com>
2015-11-03 10:08:01 -0300

    Guarda plot y aprueba pep8 en p1

--- p1.py
+++ p1.py
@@ -16,113 +16,104 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def inicializa_n(n, N_steps, h):
+def inicializa_n(n, steps_x, h):
     '''
     Rellena n con las condiciones iniciales del problema.
     Se asegura que las condiciones en los bordes sean cero.
     '''
-    for i in range(N_steps):
+    for i in range(steps_x):
         x = i * h
         n[i] = np.exp(-x**2 / 0.1)
     n[0] = 1
     n[-1] = 0
 
 
-def calcula_b(b, N_steps, r, u=1.5):
-    for j in range(1, N_steps - 1):
-        #b[j] = r * n[j+1] + 2 * (1-r) * n[j] + r * n[j-1]
-        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] + n[j] * (dt * u - dt * u * n[j])
+def calcula_b(b, steps_x, r, mu=1.5):
+    for j in range(1, steps_x - 1):
+        # b[j] = r * n[j+1] + 2 * (1-r) * n[j] + r * n[j-1]
+        # b[j] = r * n[j+1] + (1-2*r) * n[j] +
+                # r * n[j-1] + n[j] * (dt * mu- dt * mu* n[j])
+        b[j] = r * n[j+1] + (2 - 2 * r +
+                             dt * mu * (1 - n[j])) * n[j] + r * n[j-1]
 
 
-def calcula_alpha_y_beta(alpha, beta, b, r, N_Steps):
+def calcula_alfa_y_beta(alfa, beta, b, r, steps_x):
     Aplus = -1 * r
-    Acero = (1 + 2 * r)
+    Acero = (2 + 2 * r)
     Aminus = -1 * r
-    alpha[0] = 0
+    alfa[0] = 0
     beta[0] = 1  # viene de la condicion de borde n(t, 0) = 1
-    for i in range(1, N_steps):
-        alpha[i] = -Aplus / (Acero + Aminus*alpha[i-1])
-        beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
+    for i in range(1, steps_x):
+        alfa[i] = -Aplus / (Acero + Aminus*alfa[i-1])
+        beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alfa[i-1] + Acero)
 
 
-def avanza_paso_temporal(n, n_next, alpha, beta, N_steps, N_pasos_temporales, u = 1.5,):
+def avanza_paso_temporal(n, n_next, alfa, beta, steps_x, steps_t):
     n_next[0] = 1
     n_next[-1] = 0
 
-    for i in range(N_steps - 2, 0, -1):
-        n_next[i] = alpha[i] * n_next[i+1] + beta[i]
-
+    for i in range(steps_x - 2, 0, -1):
+        n_next[i] = alfa[i] * n_next[i+1] + beta[i]
 
 
 # Main
 
 # setup
+gamma = 0.001
+mu = 1.5
 ti = 0
-tf = 5
+tf = 100
 xi = 0
 xf = 1
 
-steps_t = 100
+steps_t = 10000
 steps_x = 400
-N_steps = steps_x
-N_pasos_temporales = steps_t
 
 dt = (tf - ti) / (steps_t - 1)
-dx = (xf - xi) / (steps_x - 1) #=h
+h = (xf - xi) / (steps_x - 1)
 
-#%N_steps = 41
-#N_pasos_temporales = 100
-
-#h = 1 / (N_steps - 1)
 # dt = h**2 / 2 # Este es el máximo teórico para el metodo explicito
-#dt = 0.01
-r = dt / (2 * dx**2)
-r = r * 0.001
+# Máximo teórico no funciona
 
-n = np.zeros(N_steps)
-n_next = np.zeros(N_steps)
+r = dt / (2 * h**2)
+r = r * gamma
 
-b = np.zeros(N_steps)
-alpha = np.zeros(N_steps)
-beta = np.zeros(N_steps)
+n = np.zeros(steps_x)
+n_next = np.zeros(steps_x)
 
-inicializa_n(n, N_steps, dx)
+b = np.zeros(steps_x)
+alfa = np.zeros(steps_x)
+beta = np.zeros(steps_x)
+
+inicializa_n(n, steps_x, h)
 
 # Queremos guardar las soluciones en cada paso
-n_solucion = np.zeros((N_pasos_temporales, N_steps))
+n_solucion = np.zeros((steps_t, steps_x))
 n_solucion[0, :] = n.copy()
 
-for i in range(1, N_pasos_temporales):
-    calcula_b(b, N_steps, r)
-    calcula_alpha_y_beta(alpha, beta, b, r, N_steps)
-    avanza_paso_temporal(n, n_next, alpha, beta, N_steps, N_pasos_temporales)
+for i in range(1, steps_t):
+    calcula_b(b, steps_x, r)
+    calcula_alfa_y_beta(alfa, beta, b, r, steps_x)
+    avanza_paso_temporal(n, n_next, alfa, beta, steps_x, steps_t)
     n = n_next.copy()
     n_solucion[i, :] = n.copy()
 
 
 # Plots
-
-
-x = np.linspace(0, 1, N_steps)
+x = np.linspace(0, xf, steps_x)
 
 fig = plt.figure(1)
 fig.clf()
 ax = fig.add_subplot(111)
 
-for i in range(0, N_pasos_temporales, 10):
+for i in range(0, steps_t, tf):
     ax.plot(x, n_solucion[i, :])
 ax.set_ylim(0, 1)
 
-
-# usar el plano x, t y plotear n en la 3a dimension
-fig2 = plt.figure(2)
-fig2.clf()
-ax2 = fig2.add_subplot(111)
-y = np.arange(0, N_pasos_temporales) * dt
-X, Y = np.meshgrid(x, y)
-ax2.pcolormesh(X, Y, n_solucion)
-
-
+plt.xlabel('x')
+plt.ylabel('n (x)')
+plt.title("Tiempo final = %g" % tf)
+plt.savefig("p1_tf%g.png" % tf)
 
 plt.show()
 plt.draw()
