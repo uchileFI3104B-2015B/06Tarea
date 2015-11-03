97e6a8b4abd9a370e445f6b3a09620f1f5745aae
[Felipe Castillo] <felipe.castillo.t@ing.uchile.cl>
2015-11-02 21:39:16 -0300

    Se cambiaron variables T por N, se incluye Euler

--- Solution_to_Fisher-KPP.py
+++ Solution_to_Fisher-KPP.py
@@ -10,21 +10,21 @@ import matplotlib.pyplot as plt
 
 #-implementar Crank-N
 
-def inicializa_T(T, N_steps, h):
+def inicializa_N(N, N_steps, h):
     '''
-    Rellena T con las condiciones iniciales del problema.
-    Se asegura que las condiciones en los bordes sean cero.
+    Rellena N con las condiciones iniciales del problema.
+    Se asegura que las condiciones en los bordes sean uno y cero.
     '''
     for i in range(N_steps):
         x = i * h
-        T[i] = np.exp((-x**2) / 0.1) 
-    T[0] = 0
-    T[-1] = 0
+        N[i] = np.exp((-x**2) / 0.1)
+    N[0] = 1
+    N[-1] = 0
 
 
 def calcula_b(b, N_steps, r):
     for j in range(1, N_steps - 1):
-        b[j] = r * T[j+1] + (1-2*r) * T[j] + r * T[j-1]
+        b[j] = r * N[j+1] + (1-2*r) * N[j] + r * N[j-1]
 
 
 def calcula_alpha_y_beta(alhpa, beta, b, r, N_Steps):
@@ -32,17 +32,18 @@ def calcula_alpha_y_beta(alhpa, beta, b, r, N_Steps):
     Acero = (1 + 2 * r)
     Aminus = -1 * r
     alpha[0] = 0
-    beta[0] = 0  # viene de la condicion de borde T(t, 0) = 0
+    beta[0] = 1  # viene de la condicion de borde T(t, 0) = 1 ¿?
     for i in range(1, N_steps):
         alpha[i] = -Aplus / (Acero + Aminus*alpha[i-1])
         beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
 
 
-def avanza_paso_temporal(T, T_next, alpha, beta, N_steps):
-    T_next[0] = 0
-    T_next[-1] = 0
+def avanza_paso_temporal(N, N_next, alpha, beta, N_steps):
+    N_next[0] = 1
+    N_next[-1] = 0
     for i in range(N_steps - 2, 0, -1):
-        T_next[i] = alpha[i] * T_next[i+1] + beta[i]
+        N_next_Euler[i] = N[i] + dt*mu*(N[i]-(N[i])**2)
+        N_next[i] = alpha[i] * N_next[i+1] + beta[i]
 
 #-implementar Euler explícito
 #-Main, Condiciones iniciales, condiciones de bordes, busca y guarda las soluciones en cada paso, plotea
