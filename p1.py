4b114a52d9f1509060850ee75722856942d1e206
TatiFlores <tatiflores.4@gmail.com>
2015-11-03 00:36:44 -0300

    agrega euler explicito

--- p1.py
+++ p1.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 '''
-Este script resuelve un problema simple de diffusion en 1D.
+Este script resuelve un problema simple de difusion en 1D.
 La ecuación a resover es:
-    dn/dt = gama * d2n/dx2; n(0,x) = exp(-x^2 / 0.1);
-    n(t, 0) = 1 ; n(t, 1) = 0
+    dn/dt = gama * d2n/dx2 + un - un^2;
+    C.I : n(0,x) = exp(-x^2 / 0.1);
+    C.B : n(t, 0) = 1 ; n(t, 1) = 0
+
+La parte difusiva de la ecuación se utiliza el método de Crack-Nicolson
+y la parte reactiva con el método de Euler explícito
 '''
 
 from __future__ import division
@@ -26,14 +28,15 @@ def inicializa_n(n, N_steps, h):
     n[-1] = 0
 
 
-def calcula_b(b, N_steps, r):
+def calcula_b(b, N_steps, r, u=1.5):
     for j in range(1, N_steps - 1):
-        b[j] = r * n[j+1] + 2 * (1-r) * n[j] + r * n[j-1]
+        #b[j] = r * n[j+1] + 2 * (1-r) * n[j] + r * n[j-1]
+        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] + n[j] * (dt * u - dt * u * n[j])
 
 
 def calcula_alpha_y_beta(alpha, beta, b, r, N_Steps):
     Aplus = -1 * r
-    Acero = 2 * (1 + 1 * r)
+    Acero = (1 + 2 * r)
     Aminus = -1 * r
     alpha[0] = 0
     beta[0] = 1  # viene de la condicion de borde n(t, 0) = 1
@@ -42,23 +45,39 @@ def calcula_alpha_y_beta(alpha, beta, b, r, N_Steps):
         beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
 
 
-def avanza_paso_temporal(n, n_next, alpha, beta, N_steps):
-    n_next[0] = 0
+def avanza_paso_temporal(n, n_next, alpha, beta, N_steps, N_pasos_temporales, u = 1.5,):
+    n_next[0] = 1
     n_next[-1] = 0
+
     for i in range(N_steps - 2, 0, -1):
         n_next[i] = alpha[i] * n_next[i+1] + beta[i]
-    # T = T_next.copy() # Esto no funciona, hacerlo fuera de la funcion
+
+
 
 # Main
 
 # setup
-N_steps = 41
-N_pasos_temporales = 100
+ti = 0
+tf = 5
+xi = 0
+xf = 1
 
-h = 1 / (N_steps - 1)
+steps_t = 100
+steps_x = 400
+N_steps = steps_x
+N_pasos_temporales = steps_t
+
+dt = (tf - ti) / (steps_t - 1)
+dx = (xf - xi) / (steps_x - 1) #=h
+
+#%N_steps = 41
+#N_pasos_temporales = 100
+
+#h = 1 / (N_steps - 1)
 # dt = h**2 / 2 # Este es el máximo teórico para el metodo explicito
-dt = 0.01
-r = dt / 2 / h**2
+#dt = 0.01
+r = dt / (2 * dx**2)
+r = r * 0.001
 
 n = np.zeros(N_steps)
 n_next = np.zeros(N_steps)
@@ -67,7 +86,7 @@ b = np.zeros(N_steps)
 alpha = np.zeros(N_steps)
 beta = np.zeros(N_steps)
 
-inicializa_n(n, N_steps, h)
+inicializa_n(n, N_steps, dx)
 
 # Queremos guardar las soluciones en cada paso
 n_solucion = np.zeros((N_pasos_temporales, N_steps))
@@ -76,14 +95,13 @@ n_solucion[0, :] = n.copy()
 for i in range(1, N_pasos_temporales):
     calcula_b(b, N_steps, r)
     calcula_alpha_y_beta(alpha, beta, b, r, N_steps)
-    avanza_paso_temporal(n, n_next, alpha, beta, N_steps)
+    avanza_paso_temporal(n, n_next, alpha, beta, N_steps, N_pasos_temporales)
     n = n_next.copy()
     n_solucion[i, :] = n.copy()
 
 
 # Plots
 
-# ejemplo 1
 
 x = np.linspace(0, 1, N_steps)
 
@@ -95,14 +113,16 @@ for i in range(0, N_pasos_temporales, 10):
     ax.plot(x, n_solucion[i, :])
 ax.set_ylim(0, 1)
 
-# ejemplo 2
-# usar el plano x, t y plotear T en la 3a dimension
+
+# usar el plano x, t y plotear n en la 3a dimension
 fig2 = plt.figure(2)
 fig2.clf()
 ax2 = fig2.add_subplot(111)
 y = np.arange(0, N_pasos_temporales) * dt
 X, Y = np.meshgrid(x, y)
 ax2.pcolormesh(X, Y, n_solucion)
+
+
 
 plt.show()
 plt.draw()
