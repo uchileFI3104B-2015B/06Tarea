85593faa5597eb657bc96fb27c405e712cf20b22
TatiFlores <tatiflores.4@gmail.com>
2015-11-03 16:08:45 -0300

    p2 aprueba pep8 y guarda plots

--- p2.py
+++ p2.py
@@ -4,7 +4,7 @@
 Este script resuelve un problema simple de difusion en 1D.
 La ecuación a resover es:
     dn/dt = gama * d2n/dx2 + un - un^3;
-    C.I : n(0,x) = np.random.uniform(low=-0.3, high=0.3, size=N_steps);
+    C.I : n(0,x) = np.random.uniform(low=-0.3, high=0.3, size=steps_x);
     C.B : n(t, 0) = n(t, 1) = 0
 
 La parte difusiva de la ecuación se utiliza el método de Crack-Nicolson
@@ -16,49 +16,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def inicializa_n(n, N_steps, h):
+def inicializa_n(n, steps_x, h):
     '''
     Rellena n con las condiciones iniciales del problema.
     Se asegura que las condiciones en los bordes sean cero.
     '''
-    func_random = np.random.uniform(low=-0.3, high=0.3, size=N_steps)
-    for i in range(N_steps):
-        #x = i * h
+    func_random = np.random.uniform(low=-0.3, high=0.3, size=steps_x)
+    for i in range(steps_x):
         n[i] = func_random[i]
     n[0] = 0
     n[-1] = 0
 
 
-def calcula_b(b, N_steps, r, u=1.5):
-    for j in range(1, N_steps - 1):
-        #b[j] = r * n[j+1] + 2 * (1-r) * n[j] + r * n[j-1]
-        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] + n[j] * (dt * u - dt * u * n[j]**2)
+def calcula_b(b, steps_x, r, u=1.5):
+    for j in range(1, steps_x - 1):
+        # b[j] = r * n[j+1] + 2 * (1-r) * n[j] + r * n[j-1]
+        b[j] = (r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] +
+                n[j] * (dt * u - dt * u * n[j]**2))
 
 
-def calcula_alpha_y_beta(alpha, beta, b, r, N_Steps):
+def calcula_alfa_y_beta(alfa, beta, b, r, steps_x):
     Aplus = -1 * r
     Acero = (1 + 2 * r)
     Aminus = -1 * r
-    alpha[0] = 0
+    alfa[0] = 0
     beta[0] = 0  # viene de la condicion de borde n(t, 0) = 1
-    for i in range(1, N_steps):
-        alpha[i] = -Aplus / (Acero + Aminus*alpha[i-1])
-        beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
+    for i in range(1, steps_x):
+        alfa[i] = -Aplus / (Acero + Aminus*alfa[i-1])
+        beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alfa[i-1] + Acero)
 
 
-def avanza_paso_temporal(n, n_next, alpha, beta, N_steps, N_pasos_temporales, u = 1.5,):
+def avanza_paso_temporal(n, n_next, alfa, beta, steps_x, steps_t):
     n_next[0] = 0
     n_next[-1] = 0
 
-    for i in range(N_steps - 2, 0, -1):
-        n_next[i] = alpha[i] * n_next[i+1] + beta[i]
-
+    for i in range(steps_x - 2, 0, -1):
+        n_next[i] = alfa[i] * n_next[i+1] + beta[i]
 
 
 # Main
 
 # setup
-np.random.seed(1)
+seteo = 53849
+np.random.seed(seteo)
+gamma = 0.001
+mu = 1.5
 ti = 0
 tf = 5
 xi = 0
@@ -66,65 +68,52 @@ xf = 1
 
 steps_t = 1000
 steps_x = 1000
-N_steps = steps_x
-N_pasos_temporales = steps_t
 
 dt = (tf - ti) / (steps_t - 1)
-dx = (xf - xi) / (steps_x - 1) #=h
+h = (xf - xi) / (steps_x - 1)
 
-#%N_steps = 41
-#N_pasos_temporales = 100
-
-#h = 1 / (N_steps - 1)
 # dt = h**2 / 2 # Este es el máximo teórico para el metodo explicito
-#dt = 0.01
-r = dt / (2 * dx**2)
-r = r * 0.001
+# Máximo teórico no funciona
 
-n = np.zeros(N_steps)
-n_next = np.zeros(N_steps)
+r = dt / (2 * h**2)
+r = r * gamma
 
-b = np.zeros(N_steps)
-alpha = np.zeros(N_steps)
-beta = np.zeros(N_steps)
+n = np.zeros(steps_x)
+n_next = np.zeros(steps_x)
 
-inicializa_n(n, N_steps, dx)
+b = np.zeros(steps_x)
+alfa = np.zeros(steps_x)
+beta = np.zeros(steps_x)
+
+inicializa_n(n, steps_x, h)
 
 # Queremos guardar las soluciones en cada paso
-n_solucion = np.zeros((N_pasos_temporales, N_steps))
+n_solucion = np.zeros((steps_t, steps_x))
 n_solucion[0, :] = n.copy()
 
-for i in range(1, N_pasos_temporales):
-    calcula_b(b, N_steps, r)
-    calcula_alpha_y_beta(alpha, beta, b, r, N_steps)
-    avanza_paso_temporal(n, n_next, alpha, beta, N_steps, N_pasos_temporales)
+for i in range(1, steps_t):
+    calcula_b(b, steps_x, r)
+    calcula_alfa_y_beta(alfa, beta, b, r, steps_x)
+    avanza_paso_temporal(n, n_next, alfa, beta, steps_x, steps_t)
     n = n_next.copy()
     n_solucion[i, :] = n.copy()
 
 
-# Plots
-
-
-x = np.linspace(0, 1, N_steps)
+# Plot
+x = np.linspace(0, xf, steps_x)
 
 fig = plt.figure(1)
 fig.clf()
 ax = fig.add_subplot(111)
 
-for i in range(0, N_pasos_temporales, 10):
+for i in range(0, steps_t, tf):
     ax.plot(x, n_solucion[i, :])
 ax.set_ylim(0, 1)
 
-
-# usar el plano x, t y plotear n en la 3a dimension
-fig2 = plt.figure(2)
-fig2.clf()
-ax2 = fig2.add_subplot(111)
-y = np.arange(0, N_pasos_temporales) * dt
-X, Y = np.meshgrid(x, y)
-ax2.pcolormesh(X, Y, n_solucion)
-
-
+plt.xlabel('x')
+plt.ylabel('n (x)')
+plt.title("Tiempo final = {tf}, Semilla = {seteo}".format(tf=tf, seteo=seteo))
+plt.savefig("p2_tf{tf}_setrandom{seteo}.png".format(tf=tf, seteo=seteo))
 
 plt.show()
 plt.draw()
