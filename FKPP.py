ca1f91f29386f20247ce387cf702748dbab45b7d
BScheihing <bruno.scheihing@ing.uchile.cl>
2015-11-02 22:46:12 -0300

    Cambios a graficos y agrega docstrings

--- FKPP.py
+++ FKPP.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+Este script resuelve la ecuación de Fisher-Kolmogorov-Petrovsky-Piscounov,
+usando la discretización de Crank-Nicolson para la parte difusiva de la
+ecuación y Euler explícito para la parte reactiva.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,12 +19,23 @@ def set_condicion_inicial(n, h, Lx):
 
 
 def calculo_b(n, b, r, dt):
+    '''
+    Este método calcula el lado derecho de la solución usando Crank-Nicolson
+    (es decir, los términos dependientes del instante temporal anterior).
+    Recibe como parámetros los arreglos del campo n(x,t) en el insatante
+    anterior y el arreglo b para guardar la información del lado derecho.
+    El método modifica el arreglo y no retorna valor alguno.
+    '''
     for i in range(1, len(b)-1):
         b[i] = r*n[i+1] + (1-2*r)*n[i] + r*n[i-1] + dt*(n[i] - n[i]**2)
     pass
 
 
 def calc_alpha_beta(alpha, beta, b, r):
+    '''
+    Cálculo de arreglos para alpha y beta, que son el par de coeficientes que
+    se usa para resolver cada paso temporal de la EDP.
+    '''
     A_mas = -1 * r
     A_cer = (1 + 2 * r)
     A_men = -1 * r
@@ -31,6 +48,9 @@ def calc_alpha_beta(alpha, beta, b, r):
 
 
 def dt_step(n, n_sig, alpha, beta):
+    '''
+    Avanza un paso temporal en la solución.
+    '''
     n_sig[0] = 1
     n_sig[-1] = 0
     for i in range(len(n) - 2, 0, -1):
@@ -47,8 +67,9 @@ h = 0.002*L_x
 T = 7*mu
 dt = (h**2)/4.0
 N_t = int(np.ceil(T/dt))
-
 r = dt/(2*h**2)
+
+# Condición inicial y arreglos para guardar información
 n = np.zeros(N_x)
 set_condicion_inicial(n, h, L_x)
 n_sig = np.zeros(N_x)
@@ -58,6 +79,7 @@ b = np.zeros(N_x)
 n_sol = np.zeros((N_t, N_x))
 n_sol[0, :] = n.copy()
 
+# Solución
 for i in range(1, N_t):
     calculo_b(n, b, r, dt)
     calc_alpha_beta(alpha, beta, b, r)
@@ -65,6 +87,7 @@ for i in range(1, N_t):
     n = n_sig.copy()
     n_sol[i, :] = n.copy()
 
+# Plots
 x = np.linspace(0, 1, N_x)
 fig = plt.figure(1)
 fig.clf()
@@ -74,7 +97,7 @@ for i in range(0, N_t, 100):
     if i == 0:
         ax.plot(x, n_sol[i, :], color='b', label='$n(x,t_0)$ (tiempo fijo)')
     else:
-        ax.plot(x, n_sol[i, :], color='b')
+        ax.plot(x, n_sol[i, :], color=(0, (1.0*i)/N_t, 1-(1.0*i)/N_t))
 for j in range(50, N_x - 50, 50):
     ax.arrow(x[j], n_sol[100, j], 0.0,
              2.9*(n_sol[6900, j] - n_sol[100, j])/3 - 0.05, head_width=0.025,
--- NWS.py
+++ NWS.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+Este script resuelve la ecuación de Newell-Whitehead-Segel, usando la
+discretización de Crank-Nicolson para la parte difusiva de la ecuación y
+Euler explícito para la parte reactiva.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,12 +15,23 @@ np.random.seed(347)
 
 
 def calculo_b(n, b, r, dt):
+    '''
+    Este método calcula el lado derecho de la solución usando Crank-Nicolson
+    (es decir, los términos dependientes del instante temporal anterior).
+    Recibe como parámetros los arreglos del campo n(x,t) en el insatante
+    anterior y el arreglo b para guardar la información del lado derecho.
+    El método modifica el arreglo y no retorna valor alguno.
+    '''
     for i in range(1, len(b)-1):
         b[i] = r*n[i+1] + (1-2*r)*n[i] + r*n[i-1] + dt*(n[i] - n[i]**3)
     pass
 
 
 def calc_alpha_beta(alpha, beta, b, r):
+    '''
+    Cálculo de arreglos para alpha y beta, que son el par de coeficientes que
+    se usa para resolver cada paso temporal de la EDP.
+    '''
     A_mas = -1 * r
     A_cer = (1 + 2 * r)
     A_men = -1 * r
@@ -27,6 +44,9 @@ def calc_alpha_beta(alpha, beta, b, r):
 
 
 def dt_step(n, n_sig, alpha, beta):
+    '''
+    Avanza un paso temporal en la solución.
+    '''
     n_sig[0] = 0
     n_sig[-1] = 0
     for i in range(len(n) - 2, 0, -1):
@@ -44,12 +64,12 @@ dt = (h**2)/4.0
 N_t = int(np.ceil(T/dt))
 r = dt/(2*h**2)
 
-# Condicion inicial
+# Condición inicial
 n = np.random.uniform(low=-0.3, high=0.3, size=N_x)
 n[0] = 0
 n[-1] = 0
 
-
+# Arreglos para guardar información
 n_sig = np.zeros(N_x)
 alpha = np.zeros(N_x)
 beta = np.zeros(N_x)
@@ -57,6 +77,7 @@ b = np.zeros(N_x)
 n_sol = np.zeros((N_t, N_x))
 n_sol[0, :] = n.copy()
 
+# Solución
 for i in range(1, N_t):
     calculo_b(n, b, r, dt)
     calc_alpha_beta(alpha, beta, b, r)
@@ -64,6 +85,7 @@ for i in range(1, N_t):
     n = n_sig.copy()
     n_sol[i, :] = n.copy()
 
+# Plots
 x = np.linspace(0, 1, N_x)
 fig = plt.figure(1)
 fig.clf()
@@ -73,7 +95,7 @@ for i in range(0, N_t, 100):
     if i == 0:
         ax.plot(x, n_sol[i, :], color='b', label='$n(x,t_0)$ (tiempo fijo)')
     else:
-        ax.plot(x, n_sol[i, :], color='b')
+        ax.plot(x, n_sol[i, :], color=(0, (1.0*i)/N_t, 1-(1.0*i)/N_t))
 for j in range(50, N_x - 50, 50):
     ax.arrow(x[j], n_sol[6900, j]/3 + 2*n_sol[100, j]/3, 0.0,
              1*(n_sol[6900, j] - n_sol[100, j])/3, head_width=0.025,
@@ -81,7 +103,7 @@ for j in range(50, N_x - 50, 50):
 ax.set_xlabel('$x$')
 ax.set_ylabel('$n(x,t)$')
 ax.legend()
-ax.set_title('Curvas de $n(x,t)$ para valores fijos de $t$')
+ax.set_title('Curvas de $n(x,t)$ para valores fijos de $t$, random seed = 347')
 ax.set_ylim([-1.05, 1.3])
 plt.savefig('NWS1.eps')
 plt.show()
