b5894cceec5546622dd28f66e1a7e34a04102e35
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-04 00:13:01 -0400

    inicializacion

---
+++ parte1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''
+Este script resuelve la ecuacion de Fisher-KPP
+La ecuación a resover es:
+    dn/dt = gamma * d2n/dx2 + mu * n - mu * n ** 2
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+# funciones base
+
+
+
+# main
+# inicializacion
+
+# iteracion
+
+# plots
