2842896ad322d7ed1cd27eeb6c09e43261d59ad1
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-11-03 22:42:39 -0300

    Se definen arreglos y funciones para usar C-N.

--- FKPP.py
+++ FKPP.py
@@ -6,19 +6,37 @@ Created on Tue Nov 03 20:15:45 2015
 """
 
 '''
-Este script resuelve la ecuación de Fisher-KPP, usando
-la discretización de Crank-Nicolson para la parte difusiva de la
+Este script resuelve la ecuación de Fisher-iPP, usando
+la discretización de Crani-Nicolson para la parte difusiva de la
 ecuación y Euler explícito para la parte reactiva.
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-def definir_condicion_inicial(n, h, L_x):
+def definir_condiciones_borde(n, h, L_x):
     for i in range(len(n)):
         n[i] = np.exp(-(h/L_x * i)**2 / 0.1)
     n[len(n)-1] = 0
     pass
+
+def calcular_b(n, b, r, dt):
+    '''Calcula el lado derecho (todo lo que depende del tiempo anterior
+    al que se quiere calcular), para cada punto i'''
+    for i in range(1, len(b)-1):
+        b[i] = r*n[i+1] + (1-2*r)*n[i] + r*n[i-1] + dt*(n[i] - n[i]**2)
+    pass
+
+def calcular_alpha_y_beta(alpha, beta, b, r):
+    '''Calcula todos los alphas y betas para el tiempo t dado'''
+    Amas = -1 * r
+    Acero = (1 + 2 * r)
+    Amenos = -1 * r
+    alpha[0] = 0
+    beta[0] = 1  # viene de la condicion de borde n(t, 0) = 1
+    for i in range(1, len(b)):
+        alpha[i] = -Amas / (Acero + Amenos*alpha[i-1])
+        beta[i] = (b[i] - Amenos*beta[i-1]) / (Amenos*alpha[i-1] + Acero)
 
 #Main
 
@@ -33,3 +51,12 @@ T = 7 * mu
 dt = (h**2) / 4.0
 N_t = int(np.ceil(T/dt))
 r = dt / (2*h**2)
+#C.Iniciales y definicion de arreglos
+n_t = np.zeros(N_x)
+definir_condiciones_borde(n_t, h, L_x)
+n_tsig = np.zeros(N_x)
+alpha = np.zeros(N_x)
+beta = np.zeros(N_x)
+b = np.zeros(N_x)
+n_sol = np.zeros((N_t, N_x))
+n_sol[0, :] = n_t.copy()
