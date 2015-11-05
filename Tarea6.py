2834c6f32bf477110d46597a55564c26aaf148e8
maxidirk <maxidirk@yahoo.es>
2015-11-04 21:59:34 -0300

    se completan los parametros faltantes y se hace un grafico de prueba siguiendo el ejemplo del profesor

--- Tarea6.py
+++ Tarea6.py
@@ -26,12 +26,12 @@ def inicializa_n(n, N_pasos, h):
         x = i * h
         n[i] = np.exp((-x**2)/0.1)
     n[0] = 1
-    n[1] = 0
+    n[-1] = 0
 
 
-def calcula_b(b, N_pasos, r):
+def calcula_b(b, N_pasos, r, e):
     for j in range(1, N_pasos - 1):
-        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1]
+        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] + e*(n[j] - n[j]**2)
 
 
 def calcula_alpha_y_beta(alhpa, beta, b, r, N_pasos):
@@ -39,14 +39,14 @@ def calcula_alpha_y_beta(alhpa, beta, b, r, N_pasos):
     Acero = (1 + 2 * r)
     Aminus = -1 * r
     alpha[0] = 0
-    beta[0] = 0  # viene de la condicion de borde T(t, 0) = 0
+    beta[0] = 1  # viene de la condicion de borde n(t, 0) = 1
     for i in range(1, N_pasos):
         alpha[i] = -Aplus / (Acero + Aminus*alpha[i-1])
         beta[i] = (b[i] - Aminus*beta[i-1]) / (Aminus*alpha[i-1] + Acero)
 
 
 def avanza_paso_temporal(n, n_next, alpha, beta, N_pasos):
-    n_next[0] = 0
+    n_next[0] = 1
     n_next[-1] = 0
     for i in range(N_pasos - 2, 0, -1):
         n_next[i] = alpha[i] * n_next[i+1] + beta[i]
@@ -67,23 +67,51 @@ n(t,1) = 0
 n(0,x) = exp(-x**2 / 0.1)
 integrar para almenos t=4
 '''
-N_pasos = 500   #numero de pasos espaciales
-h = 1./N_pasos  #tamanho del paso espacial
-e = 0.005       #tamanho del paso espacial
+N_pasos = 500         #numero de pasos espaciales
+h = 1. / (N_pasos-1)  #tamanho del paso espacial #verificar el -1
+e = 0.005             #tamanho del paso espacial
+r = e / (2. * h**2)
+N_pasos_temporales = int(4. / e)    #numero de pasos en el tiempo
 
 
+n = np.zeros(N_pasos)
+n_next = np.zeros(N_pasos)
 
+b = np.zeros(N_pasos)
+alpha = np.zeros(N_pasos)
+beta = np.zeros(N_pasos)
 
+inicializa_n(n, N_pasos, h)
+
+# Queremos guardar las soluciones en cada paso
+n_solucion = np.zeros((N_pasos_temporales, N_pasos))
+n_solucion[0, :] = n.copy()
+
+for i in range(1, N_pasos_temporales):
+    calcula_b(b, N_pasos, r, h)
+    calcula_alpha_y_beta(alpha, beta, b, r, N_pasos)
+    avanza_paso_temporal(n, n_next, alpha, beta, N_pasos)
+    n = n_next.copy()
+    n_solucion[i, :] = n.copy()
+
+x = np.linspace(0, 1, N_pasos)
+
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(111)
+
+for i in range(0, N_pasos_temporales, 10):
+    ax.plot(x, n_solucion[i, :])
+ax.set_ylim(0, 1)
+
+plt.show()
+plt.draw()
 ##############################################################################
 ##############################################################################
 #Desarrollo P2
 '''
 dn/dt = g*d2n/dx2 + mn - mn3   n(x,t)
 '''
-
-##############################################################################
-##############################################################################
-#Graficos
 
 ##############################################################################
 ##############################################################################
