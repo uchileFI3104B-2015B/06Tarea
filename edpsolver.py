1ef5d57bf3e2a4d26c3864b0cf7bd9c970bf1f5f
nicokurtovic <nicokurtovic@gmail.com>
2015-11-05 00:27:12 -0300

    finalizado codigo y verificado para problema 1

--- edpsolver.py
+++ edpsolver.py
@@ -25,14 +25,14 @@ import numpy as np
 
 '''
 Este codigo resuelve una EDP de la forma de la ecuacion del calor mas un 
-termino adicional f(m). m es un vecto m[espacio][tiempo]
+termino adicional f(m). m es un vecto m[espacio,tiempo]
 '''
 
 def reaccion(m, i, n, mu):
   '''
   Devuelve la funcion de reaccion para m_i**n
   '''
-  return mu * m[i][n] * (1. - m[i][n])
+  return mu * m[i,n] * (1. - m[i,n])
 
 
 def crear_m_zeros(nx, nt):
@@ -45,25 +45,28 @@ def rellenar_b(m, i, n, dt, dx, gamma, mu):
   '''
   eta = (gamma * dt) / (2. * dx**2)  
   if i == 0:
-    return eta * (m[i+1][n] - m[i][n]) + m[i][n] + dt * reaccion(m, i, n, mu)
+    return eta * (m[i+1,n] - m[i,n]) + m[i,n] + dt * reaccion(m, i, n, mu)
   
-  elif i == (len(m[:][0]) - 1):
-    return eta * (-2. * m[i][n] + m[i-1][n]) + m[i][n] + dt * reaccion(m, i, n, mu)
+  elif i == (len(m[:,0]) - 1):
+    return eta * (-2. * m[i,n] + m[i-1,n]) + m[i,n] + dt * reaccion(m, i, n, mu)
 
   else:
-    return eta * (m[i+1][n] - 2 * m[i][n] + m[i-1][n]) + m[i][n] + dt * reaccion(m, i, n, mu)
+    return eta * (m[i+1,n] - 2 * m[i,n] + m[i-1,n]) + m[i,n] + dt * reaccion(m, i, n, mu)
 
 
 def b_vector(m, n, dt, dx, gamma, mu):
   '''
-  Crea un vector del largo de m[:][n] con la forma de la solucion para CN
+  Crea un vector del largo de m[:,n] con la forma de la solucion para CN
   combinado con Euler
   '''
-  l = len(m[:][0])
+  l = len(m[:,0])
   b = np.zeros(l)
-  
+  eta = (gamma * dt) / (2. * dx**2) 
   for i in range(l):
-    b[i] = rellenar_b(m, i, n, dt, dx, gamma, mu)
+    if i == 0:
+      b[i] = 1. + 2. * eta
+    else:
+      b[i] = rellenar_b(m, i, n, dt, dx, gamma, mu)
   
   return b
 
@@ -72,17 +75,17 @@ def A_matriz(m, gamma, dt, dx):
   '''
   Crea la matriz A del sistema de ecuaciones
   '''
-  l = len(m[:][0])
+  l = len(m[:,0])
   A = np.zeros((l,l))
   eta = (gamma * dt) / (2. * dx**2) 
   for i in range(l):
     for j in range(l):
       if i == j:
-        A[i][j] = 1. + 2. * eta
-      if j >= 0 and j == (i - 1):
-        A[i][j] = - eta
-      if j <= (l-1) and j == (i + 1):
-        A[i][j] = - eta
+        A[i,j] = 1. + 2. * eta
+      if j == (i - 1):
+        A[i,j] = - eta
+      if j > 1 and j == (i + 1):
+        A[i,j] = - eta
   return A      
         
 
@@ -93,7 +96,7 @@ def un_paso(m, n, dt, dx, gamma, mu):
   if np.allclose(np.dot(A, x), b):
     for i in range(len(x)):
       try:
-        m[i][n+1] = np.copy(x[i])
+        m[i,n+1] = np.copy(x[i])
       except IndexError:
         break
   else:
@@ -101,9 +104,10 @@ def un_paso(m, n, dt, dx, gamma, mu):
 
 
 def set_condicion_inicial(m):
-  l = len(m[:][0])
+  l = len(m[:,0])
   for i in range(l):
-    m[i][0] = np.exp( -10. * ( float(i + 1) / float(l) )**2 )
+    m[i,0] = np.exp( -10. * ( float(i + 1) / float(l) )**2 )
+    m[0,i] = 1.
 
 
 def resolver(nt, nx, gamma, mu):
@@ -111,21 +115,51 @@ def resolver(nt, nx, gamma, mu):
   dx = 1. / nx
   m = crear_m_zeros(nx, nt)
   set_condicion_inicial(m)
-  for n in range(len(m[0][:]-1)):
+  for n in range(len(m[0,:]-1)):
     un_paso(m, n, dt, dx, gamma, mu)
+    print m[0,n]
   return m
 
 
-def graficar(V):
+def graficar1(V):
   '''
-  Grafica la matriz V
+  Grafica la matriz V en un diagrama de tonos
   '''
   p.imshow(np.arcsinh(V) , interpolation = 'nearest')#,
-#    extent=[-5, 5, -7.5, 7.5])
-#  p.xlabel('X $[cm]$')
-#  p.ylabel('Y $[cm]$')
+  #extent=[-5, 5, -7.5, 7.5])
+  p.xlabel('X $[cm]$')
+  p.ylabel('Y $[cm]$')
   p.colorbar()
   p.show()  
+
+
+def graficar2(m):
+  '''
+  Grafica V en varios subplots
+  '''
+  fig = p.figure(1)
+  fig.clf()
+  ax = fig.add_subplot(111)
+  lx = len(m[:,0])
+  x = np.linspace(0, 1, lx)
+  
+  for i in range(0, lx, 50):
+    ax.plot(x, m[:,i])
+  #ax.set_ylim(0,1)
+  
+  p.show()
+
+
+
+
+
+
+
+
+
+
+
+
 
     
 #############################################################################
@@ -139,9 +173,9 @@ nt = 500
 
 m = resolver(nt, nx, gamma, mu)
 
-graficar(m)
+graficar1(m)
 
-
+graficar2(m)
 
 
 
