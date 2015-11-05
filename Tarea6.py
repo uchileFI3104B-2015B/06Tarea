0e7f1cfdc50401b6c36f6dbc1105554f7a53dbc0
maxidirk <maxidirk@yahoo.es>
2015-11-05 00:11:00 -0300

    se hacen correcciones en la implementacion del metodo y se grafica pregunta 1

--- Tarea6.py
+++ Tarea6.py
@@ -29,10 +29,9 @@ def inicializa_n(n, N_pasos, h):
     n[-1] = 0
 
 
-def calcula_b(b, N_pasos, r, e):
+def calcula_b(b, N_pasos, r, e, mu):
     for j in range(1, N_pasos - 1):
-        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] + e*(n[j] - n[j]**2)
-
+        b[j] = r * n[j+1] + (1-2*r) * n[j] + r * n[j-1] + mu*e*(n[j] - n[j]**2)
 
 def calcula_alpha_y_beta(alhpa, beta, b, r, N_pasos):
     Aplus = -1 * r
@@ -67,28 +66,27 @@ n(t,1) = 0
 n(0,x) = exp(-x**2 / 0.1)
 integrar para almenos t=4
 '''
-N_pasos = 500         #numero de pasos espaciales
-h = 1. / (N_pasos-1)  #tamanho del paso espacial #verificar el -1
-e = 0.005             #tamanho del paso espacial
-r = e / (2. * h**2)
-N_pasos_temporales = int(4. / e)    #numero de pasos en el tiempo
+gamma = 0.001
+mu = 1.5
+N_pasos = 500               #numero de pasos espaciales
+h = 1. / (N_pasos-1)        #tamanho del paso espacial #verificar el -1
+e = 0.005                   #tamanho del paso espacial
+r = gamma * e / (2. * h**2) #cambio de variable para simplificacion del metodo
+N_pasos_temporales = int(5. / e)    #numero de pasos en el tiempo
 
 
 n = np.zeros(N_pasos)
 n_next = np.zeros(N_pasos)
-
 b = np.zeros(N_pasos)
 alpha = np.zeros(N_pasos)
 beta = np.zeros(N_pasos)
 
 inicializa_n(n, N_pasos, h)
-
-# Queremos guardar las soluciones en cada paso
 n_solucion = np.zeros((N_pasos_temporales, N_pasos))
 n_solucion[0, :] = n.copy()
 
 for i in range(1, N_pasos_temporales):
-    calcula_b(b, N_pasos, r, h)
+    calcula_b(b, N_pasos, r, e, mu)
     calcula_alpha_y_beta(alpha, beta, b, r, N_pasos)
     avanza_paso_temporal(n, n_next, alpha, beta, N_pasos)
     n = n_next.copy()
@@ -100,23 +98,24 @@ fig = plt.figure(1)
 fig.clf()
 ax = fig.add_subplot(111)
 
-for i in range(0, N_pasos_temporales, 10):
+for i in range(0, N_pasos_temporales, 20):
     ax.plot(x, n_solucion[i, :])
 ax.set_ylim(0, 1)
 
+plt.title('Densidad de la especie vs Posicion. A traves del tiempo')
+plt.xlabel('Posicion de la especie en una dimension')
+plt.ylabel('Densidad de la especie')
+
 plt.show()
 plt.draw()
+
+
 ##############################################################################
 ##############################################################################
 #Desarrollo P2
 '''
 dn/dt = g*d2n/dx2 + mn - mn3   n(x,t)
 '''
-
-##############################################################################
-##############################################################################
-#zona de pruebas
-
 
 
 
