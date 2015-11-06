33a17fa5ae59409a5e1c5c0fdbcf0a6522dd2407
Felipe Toledo <ftoledo.bit@gmail.com>
2015-11-06 02:39:09 -0300

    Solver implementado. Falta establecer casos en el main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 '''
 uso: main.py
@@ -13,7 +14,10 @@ if __name__ == '__main__':
     numero_puntos = 500.0
     largo = 1.0
     x_axis = np.linspace(0, 1, num = numero_puntos)
-
+    delta_x = x_axis[1] - x_axis[0]
+    t_ini = 0
+    t_fin = 4
+    delta_t = 0.001
 
     ''' Crear sistema P1 '''
     sys_p1 = sys.make_reaction_diffusion_system(numero_puntos)
@@ -31,4 +35,11 @@ if __name__ == '__main__':
     # Set componente de reaccion
     mu = 1.5
     reac_coef = [0, mu, -mu]
+    # reac_coef = [0]
     sys_p1.set_reac_coefficients(reac_coef)
+
+    n_p1 = sys_p1.integrate(t_ini, t_fin, delta_t, delta_x)
+
+    plt.plot(x_axis, sys_p1._n,'r')
+    plt.plot(x_axis, n_p1)
+    plt.show()
--- reaction_diffusion_system.py
+++ reaction_diffusion_system.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from numpy.linalg import *
 
 
 class reaction_diffusion_system(object):
@@ -73,40 +74,88 @@ class reaction_diffusion_system(object):
         ''' Permite escoger el valor de gamma '''
         self._gamma = gamma
 
+        print('Gamma inicializado')
+
     # END of set_gamma
 
-    def _eval_reac_poly(n):
+    def _eval_reac_poly(self, n):
         ''' Evalua polinomio de reaccion '''
-        value = 0
-        grade = 0
+        value = 0.0
+        grade = 0.0
 
-        for coef in _reac_poly:
-            value += coef * n ** grade
-            grade += 1
+        for coef in self._reac_poly:
+            value += coef * (n ** grade)
+            grade += 1.0
 
         return value
     # END of _eval_reac_poly
 
-    def _reac_solver(h, n):
+    def _reac_solver(self, h, n):
         ''' Entrega valor de y_n+1 a partir de h (intervalo temporal)
         e y_n para la componente de reaccion '''
-        return n + h * _eval_reac_poly(n)
+        return h * self._eval_reac_poly(n)
     # END of _reac_solver
 
-    def integrate(self, start_time, stop_time, step_size):
-        ''' Resuelve el sistema desde t = start_time
-        hasta stop_time, con resolucion temporal step_size
-        Retorna nuevo arreglo con situacion en stop_time '''
+    def _diff_solver(self, time_step, x_step, n):
+        ''' Integra vector n usando metodo de crank-nicolson '''
+        h = time_step
+        M = self._gamma * h / (x_step**2)
 
-        n = np.copy(_n)
-        n_next = np.copy(n)
-        h = step_size
+        # print(len(n))
+
+        len_matrix = len(n) - 2
+        A = np.zeros((len_matrix, len_matrix))
+        b = np.zeros(len_matrix)
+
+        ''' Se escribe elemento b '''
+        for i in range(len_matrix):
+            b[i] = M / 2 * n[i] + (1-M) * n[i+1] + M / 2 * n[i+2]
+
+        ''' Bordes de matriz A y vector b '''
+        A[0, 0] = (1 + M)
+        A[0, 1] = -M / 2
+        b[0] = b[0] + M / 2 * n[0]
+
+        for i in range(1, len_matrix - 1):
+            A[i, i-1] = -M / 2
+            A[i, i] = 1 + M
+            A[i, i+1] = -M / 2
+
+        A[-1, -2] = -M / 2
+        A[-1, -1] = 1 + M
+        b[-1] = b[-1] + M / 2 * n[-1]
+
+        ''' Resolver sist lineal '''
+        x = solve(A, b)
+        for i in range(0, len(x)):
+            n[i+1] = x[i]
+
+        return n
+    # END of _diff_solver
+
+    def integrate(self, start_time, stop_time, time_step, x_step):
+        ''' Resuelve el sistema desde t = start_time
+        hasta stop_time, con resolucion temporal time_step
+        Retorna nuevo arreglo con situacion en stop_time
+        x_step es el paso en unidad de largo '''
+
+        n = np.copy(self._n)
+        n_reac = np.copy(n)
+        n_diff = np.copy(n)
+        h = x_step
         t = np.arange(start_time, stop_time + h, h)
 
-        for i in range(t):
-            for j in range(1, len(n) - 1):
-                n_reac = _reac_solver(h, n[j])
+        for i in range(len(t)):
+            n_diff = self._diff_solver(h, x_step, n_diff)
 
+            for j in range(1, len(n) - 1):
+                n_reac[j] = self._reac_solver(h, n_reac[j])
+                # n[j] = n_diff[j] + n_reac[j]
+
+        for i in range(1,len(n) - 1):
+            n[i] = n_diff[i] + n_reac[i]
+
+        return n
     # END of integrate
 
 
@@ -120,7 +169,7 @@ def make_reaction_diffusion_system(num_puntos):
     ''' Permite crear un objeto de sistema reaccion-difusion '''
     new_system = reaction_diffusion_system(num_puntos)
 
-    print('Sistema con ' + str(num_puntos) + ' puntos inicializado')
+    print('Sistema de ' + str(num_puntos) + ' puntos iniciado')
 
     return new_system
 
