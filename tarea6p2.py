ddfb4c263c58840e07c7a13459ad89b982b0b2e9
brevadiaz <eva.diaz.g@ing.uchile.cl>
2015-11-05 23:34:30 -0300

    Agrego P2 y cambios chiquititos a P1

--- tarea6p2.py
+++ tarea6p2.py
@@ -1,10 +1,11 @@
 '''
-Este script resuelve la ecuación de Fisher-KPP mediante
+Este script resuelve la ecuación de Newell-Whitehead-Segel mediante
 el método de Crank-Nicolson.
 La ecuación a resolver es:
-dn/dt = gamma * d2n/dx2 + mu* n - mu* n^2
+dn/dt = gamma * d2n/dx2 + mu* (n - n^3)
 Condiciones de borde e incial:
-n(t, 0) = 1 ; n(t, 1) = 0 ; n(0, x) = exp(-x^2 / 0.1)
+n(t, 0) = 0 ; n(t, 1) = 0 ;
+n(0, x) = np.random.uniform(low=-0.3, high=0.3, size=Nx)
 '''
 
 from __future__ import division
@@ -12,17 +13,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+semilla = raw_input("Ingresar semilla:")
+np.random.seed(int(semilla))
+
 def inicializa_n(n, N_steps, dx):
     '''
     Rellena n con las condiciones iniciales del problema.
     Se asegura que las condiciones en los bordes sean cero.
     '''
+    N = np.random.uniform(low=-0.3, high=0.3, size=N_steps)
     for i in range(N_steps):
-        x = i * dx
-        n[i] = np.exp((-x**2) / 0.1)
-    n[0] = 1
+        n[i] = N[i]
+    n[0] = 0
     n[-1] = 0
-    pass
 
 def calcular_b(b, N_steps, r):
     '''
@@ -30,7 +33,7 @@ def calcular_b(b, N_steps, r):
     del instante anterior.
     '''
     for j in range(1, N_steps - 1):
-        b[j] = r*n[j+1] + (1 - 2*r)*n[j] + r*n[j-1] + dt*mu*(n[j] - n[j]**2)
+        b[j] = r*n[j+1] + ((1 - 2*r) + dt*mu*(1 - n[j]**2))*n[j] + r * n[j-1]
     pass
 
 def alpha_beta(alpha, beta, b, r, N_steps):
@@ -41,7 +44,7 @@ def alpha_beta(alpha, beta, b, r, N_steps):
     A_zero = (1 + 2 * r)
     A_minus = -1 * r
     alpha[0] = 0
-    beta[0] = 1  # viene de la condicion de borde n(t, 0) = 1
+    beta[0] = 0  # viene de la condicion de borde n(t, 0) = 0
     for i in range(1, N_steps):
         alpha[i] = -A_plus / (A_zero + A_minus * alpha[i-1])
         beta[i] = (b[i] - A_minus * beta[i-1]) / (A_minus * alpha[i-1] + A_zero)
@@ -51,15 +54,11 @@ def time_step(n, n_next, alpha, beta):
     '''
     Avanza un instante en el tiempo.
     '''
-    n_next[0] = 1 #condición de borde n(t,0) = 1
+    n_next[0] = 0 #condición de borde n(t,0) = 0
     n_next[-1] = 0 #condición de borde n(t,1) = 0
     for i in range(N_steps - 2, 0, -1):
         n_next[i] = alpha[i] * n_next[i+1] + beta[i]
     pass
-
-#Main
-
-#Setup
 
 x_i = 0
 x_f = 1
